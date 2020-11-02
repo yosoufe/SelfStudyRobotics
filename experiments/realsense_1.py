7dde33cf04e47cbdb680c84a72165bef871eb18f
Yousof <yousofebneddin@yahoo.com>
2020-11-01 23:37:27 -0800

    more experiment with realsense:

--- experiments/realsense_1.py
+++ experiments/realsense_1.py
@@ -1,6 +1,8 @@
 import pyrealsense2 as rs
+import numpy as np
+import time
 
-try:
+def main():
     # Create a context object. This object owns the handles to all connected realsense devices
     pipeline = rs.pipeline()
 
@@ -10,22 +12,32 @@ try:
     #config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     
-    colorizer = rs.colorizer()
-    pipeline.start(config)
+    profile = pipeline.start(config)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
 
     while True:
         # This call waits until a new coherent set of frames is available on a device
-        # Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
+        # Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will 
+        # return stable values until wait_for_frames(...) is called
         frames = pipeline.wait_for_frames()
         depth = frames.get_depth_frame()
-        if not depth: continue
 
-        # Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and approximating the coverage of pixels within one meter
+        if not depth: 
+            print('missing depth frame')
+            continue
+
+        depth_f = np.asanyarray(depth.get_data(), dtype= np.float)
+        distance = depth_f * depth_scale
+
+        # Print a simple text-based representation of the image, 
+        # by breaking it into 10x20 pixel regions and approximating 
+        # the coverage of pixels within one meter
         coverage = [0]*64
         to_print = ""
         for y in range(480):
             for x in range(640):
-                dist = depth.get_distance(x, y)
+                dist = distance[y, x]
                 if 0 < dist and dist < 1:
                     coverage[x//10] += 1
             
@@ -36,12 +48,7 @@ try:
                 coverage = [0]*64
                 to_print += line + '\n'
         print(to_print + '\n\n\n')
-    exit(0)
-#except rs.error as e:
-#    # Method calls agaisnt librealsense objects may throw exceptions of type pylibrs.error
-#    print("pylibrs.error was thrown when calling %s(%s):\n", % (e.get_failed_function(), e.get_failed_args()))
-#    print("    %s\n", e.what())
-#    exit(1)
-except Exception as e:
-    print(e)
-    pass
+
+
+if __name__ == "__main__":
+    main()
